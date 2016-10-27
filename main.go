15c2339d48e6080aa42d96111f9d5a25cf553819
Alessandro Puccetti <alessandro@kinvolk.io>
2016-10-27 18:38:55 +0200

    core: create scope/plugins/iowait directory for the socket.
    There is no guarantee that the probe is started before the plugin,
    so the directory `/var/run/scope/plugins` might not exist yet.
    If the `/var/run/scope/plugins/iowait` does not exist, creates it.
    The permissions are set to `0700`.

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"flag"
 	"fmt"
 	"log"
 	"net"
@@ -10,21 +9,46 @@ import (
 	"os"
 	"os/exec"
 	"os/signal"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"sync"
 	"time"
 )
 
-func main() {
-	hostname, _ := os.Hostname()
-	var (
-		addr   = flag.String("addr", "/var/run/scope/plugins/iowait.sock", "unix socket to listen for connections on")
-		hostID = flag.String("hostname", hostname, "hostname of the host running this plugin")
-	)
-	flag.Parse()
+func setupSocket(socketPath string) (net.Listener, error) {
+	os.RemoveAll(filepath.Dir(socketPath))
+	if err := os.MkdirAll(filepath.Dir(socketPath), 0700); err != nil {
+		return nil, fmt.Errorf("failed to create directory %q: %v", filepath.Dir(socketPath), err)
+	}
+	listener, err := net.Listen("unix", socketPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to listen on %q: %v", socketPath, err)
+	}
 
-	log.Printf("Starting on %s...\n", *hostID)
+	log.Printf("Listening on: unix://%s", socketPath)
+	return listener, nil
+}
+
+func setupSignals(socketPath string) {
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+	go func() {
+		<-interrupt
+		os.RemoveAll(filepath.Dir(socketPath))
+		os.Exit(0)
+	}()
+}
+
+func main() {
+	// We put the socket in a sub-directory to have more control on the permissions
+	const socketPath = "/var/run/scope/plugins/iowait/iowait.sock"
+	hostID, _ := os.Hostname()
+
+	// Handle the exit signal
+	setupSignals(socketPath)
+
+	log.Printf("Starting on %s...\n", hostID)
 
 	// Check we can get the iowait for the system
 	_, err := iowait()
@@ -32,27 +56,16 @@ func main() {
 		log.Fatal(err)
 	}
 
-	os.Remove(*addr)
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt)
-	go func() {
-		<-interrupt
-		os.Remove(*addr)
-		os.Exit(0)
-	}()
-
-	listener, err := net.Listen("unix", *addr)
+	listener, err := setupSocket(socketPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer func() {
 		listener.Close()
-		os.Remove(*addr)
+		os.RemoveAll(filepath.Dir(socketPath))
 	}()
 
-	log.Printf("Listening on: unix://%s", *addr)
-
-	plugin := &Plugin{HostID: *hostID}
+	plugin := &Plugin{HostID: hostID}
 	http.HandleFunc("/report", plugin.Report)
 	http.HandleFunc("/control", plugin.Control)
 	if err := http.Serve(listener, nil); err != nil {
